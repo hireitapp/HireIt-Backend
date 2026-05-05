7d634b4d06dd1a5fd04faf6ca1a3bbea6fd80ca8
hireitapp <ky@kcroofplumbing.com.au>
2026-05-05 18:17:00 +1000

    update stripe key variable order

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const { Resend } = require('resend')
 
-const getStripe = () => require('stripe')(process.env.HIREIT_STRIPE_KEY || process.env.STRIPE_SECRET_KEY)
+const getStripe = () => require('stripe')(process.env.STRIPE_KEY || process.env.HIREIT_STRIPE_KEY || process.env.STRIPE_SECRET_KEY)
 const resend = new Resend(process.env.RESEND_API_KEY)
 const app = express()
 
